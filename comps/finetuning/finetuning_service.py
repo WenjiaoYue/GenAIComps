4bd6912b5d7eb156e4f02cef6ce524419701fc8d
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-09-03 20:31:47 +0800

    update format

--- comps/finetuning/finetuning_service.py
+++ comps/finetuning/finetuning_service.py
@@ -66,7 +66,7 @@ def cancel_finetuning_job(request: FineTuningJobIDRequest):
     port=8015,
 )
 async def upload_training_files(
-    file: Optional[Union[UploadFile]] = File(None),
+     file: UploadFile = File(...)
 ):
     return await handle_upload_training_files(file)
 
