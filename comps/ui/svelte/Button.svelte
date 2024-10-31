2f397e8287e4288814fe1f37d0af3f6b36abcd6b
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-11-01 07:01:36 +0800

    update color

--- comps/ui/svelte/Button.svelte
+++ comps/ui/svelte/Button.svelte
@@ -1,22 +1,18 @@
 <script>
-    export let label = "Click Me";
-    export let onClick = () => {}; 
+    export let label = "Click Me!";
+    export let onClick = () => {};
+  
   </script>
   
-  <button on:click={onClick}> {label} </button>
+  <button 
+    on:click={onClick} 
+    style="padding: 10px; background-color: blue; color: white; border: none; border-radius: 5px;">
+    {label}
+  </button>
   
   <style>
-    button {
-      padding: 10px;
-      background-color: blue;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      transition: background-color 0.3s; 
-    }
-  
     button:hover {
-      background-color: rgb(0, 174, 255); 
+      background-color: darkblue;
     }
   </style>
   
