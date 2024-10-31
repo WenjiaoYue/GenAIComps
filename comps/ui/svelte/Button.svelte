309679f5a0fb8e2abda31caa9fca2195d7a77d9a
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-11-01 06:14:33 +0800

    upload svelte compose test

---
+++ comps/ui/svelte/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let label = "Click Me!";
+    export let onClick = () => {};
+  
+  </script>
+  
+  <button 
+    on:click={onClick} 
+    style="padding: 10px; background-color: blue; color: white; border: none; border-radius: 5px;">
+    {label}
+  </button>
+  
+  <style>
+    button:hover {
+      background-color: darkblue;
+    }
+  </style>
+  
